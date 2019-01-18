566e1f6e046ee684b0919181e174897e54b834d7
gnems <g_nemsveridze@cu.edu.ge>
2019-01-18 13:38:08 +0400

    Add style for form

--- registration/register.php
+++ registration/register.php
@@ -8,5 +8,28 @@
     <div class="header">
         <h2>Register</h2>
     </div>
+
+    <form method="post" action="register.php">
+        <div class="input-group">
+            <label>Username</label>
+            <input type="text" name="username">
+        </div>
+        <div class="input-group">
+            <label>Email</label>
+            <input type="text" name="email">
+        </div>
+        <div class="input-group">
+            <label>Password</label>
+            <input type="password" name="password_1">
+        </div>
+        <div class="input-group">
+            <label>Confirm Password</label>
+            <input type="password" name="password_2">
+        </div>
+        <div class="input-group">
+            <button type="submit" name="register" class="btn">Register</button>
+        </div>
+    </form>
+
 </body>
 </html>
