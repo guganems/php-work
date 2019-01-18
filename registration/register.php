dbd976a62a2f2e82355de9dfba83d062474b9213
gnems <g_nemsveridze@cu.edu.ge>
2019-01-18 13:44:17 +0400

    Finish UI

--- registration/register.php
+++ registration/register.php
@@ -29,6 +29,9 @@
         <div class="input-group">
             <button type="submit" name="register" class="btn">Register</button>
         </div>
+        <p>
+            Already a member? <a href="login.php">Sign in</a>
+        </p>
     </form>
 
 </body>
