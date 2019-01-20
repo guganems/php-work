8564593f19f57075add46b56868544a206cbcf37
gnems <g_nemsveridze@cu.edu.ge>
2019-01-20 12:24:43 +0400

    Add Login validation

--- registration/login.php
+++ registration/login.php
@@ -1,3 +1,4 @@
+<?php include ('server.php'); ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,13 +11,15 @@
 </div>
 
 <form method="post" action="login.php">
+    <!--        display validation errors here-->
+    <?php include ('errors.php') ?>
     <div class="input-group">
         <label>Username</label>
         <input type="text" name="username">
     </div>
     <div class="input-group">
         <label>Password</label>
-        <input type="password" name="password_1">
+        <input type="password" name="password">
     </div>
     <div class="input-group">
         <button type="submit" name="login" class="btn">Login</button>
