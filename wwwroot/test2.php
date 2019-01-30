5910e68ddf5f54b4681898f361fa76066c366936
gnems <g_nemsveridze@cu.edu.ge>
2019-01-30 14:40:24 +0400

    A Function that returns a Value

--- wwwroot/test2.php
+++ wwwroot/test2.php
@@ -123,6 +123,27 @@ function showmessage($message){
 
 showmessage('This message was deployed from within a function');
 
+function greetingmessage(){
+    date_default_timezone_set("Etc/GMT+4");
+    $hour = date('H');
+    $dow = date('N')-1;
+    $namesofdays = array("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday");
+
+    if ($hour >= 0 && $hour < 12){
+        $result = "Good morning";
+    }
+    elseif ($hour >= 12 && $hour < 19){
+        $result = "Good Afternoon";
+    }
+    else{
+        $result = "Good Evening";
+    }
+
+    $result = $result . ", today is $namesofdays[$dow]";
+    return $result;
+}
+
+echo greetingmessage();
 
 
 
