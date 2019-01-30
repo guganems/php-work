5afadc5c18ab0010aa291013bd707e40866157c3
gnems <g_nemsveridze@cu.edu.ge>
2019-01-30 14:10:28 +0400

    Switch statement

--- wwwroot/test2.php
+++ wwwroot/test2.php
@@ -46,15 +46,47 @@ foreach ($intnumbers as $k => $value) {
 
 date_default_timezone_set("Etc/GMT+4");
 $hour = date('H');
+$dow = date('D');
+
 if ($hour >= 0 && $hour < 12){
-    echo "Good morning!";
+    $greeting = "Good Morning!";
 }
 elseif ($hour > 12 && $hour < 19){
-    echo "Good Afternoon";
+    $greeting = "Good Afternoon";
 }
 else{
-    echo "Good Evening";
+    $greeting = "Good Evening";
 }
+
+switch ($dow){
+    case "Mon":
+        echo $greeting . ", today is Monday";
+        break;
+    case "Tue":
+        echo $greeting . ", today is Tuesday";
+        break;
+    case "Wed":
+        echo $greeting . ", today is Wednesday";
+        break;
+    case "Thu":
+        echo $greeting . ", today is Thursday";
+        break;
+    case "Fri":
+        echo $greeting . ", today is Friday";
+        break;
+    case "Sat":
+        echo $greeting . ", today is Saturday";
+        break;
+    case "Sun":
+        echo $greeting . ", today is Sunday";
+        break;
+    default:
+        echo $greeting . " What day is this?";
+}
+
+
+
+
 
 
 
