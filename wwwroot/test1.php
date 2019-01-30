e9d282d0235d64a2126d14804b370ed24727e104
gnems <g_nemsveridze@cu.edu.ge>
2019-01-30 12:18:31 +0400

    Calling HTML from PHP sample

---
+++ wwwroot/test1.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nemst
+ * Date: 30.01.2019
+ * Time: 12:14
+ */
+echo "<html lang='en-US'>\n<head>\n<title>Calling HTML from PHP</title>\n</head>\n<body>\n<h1>Hello, Calling HTML from PHP!</h1>\n</body>\n</html>";
