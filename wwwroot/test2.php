70033ece414cf997107836e9e55c22c8ccfb9332
gnems <g_nemsveridze@cu.edu.ge>
2019-01-30 15:17:58 +0400

    Calling Functions Dynamically

--- wwwroot/test2.php
+++ wwwroot/test2.php
@@ -146,6 +146,42 @@ function greetingmessage($showdayofweek = false){
 echo greetingmessage(true);
 echo greetingmessage();
 
+function getgreetingfunction(){
+    date_default_timezone_set("Etc/GMT+4");
+    $funcnames  = array("goodmorning", "goodmorning", "goodafternoon", "goodevening");
+    $hour = date('H');
+    return $funcnames[(int)$hour/6];
+}
+
+function dayofweeknames($daynumber){
+    $namesofdays = array("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday");
+    return $namesofdays[$daynumber - 1];
+}
+
+function goodmorning($showdayofweek = false){
+    $result = "Good Morning";
+    if ($showdayofweek) $result = $result . ", today is " . dayofweeknames(date('N'));
+    $result = $result . "<br />";
+    return $result;
+}
+
+function goodafternoon($showdayofweek = false){
+    $result = "Good Afternoon";
+    if ($showdayofweek) $result = $result . ", today is " . dayofweeknames(date('N'));
+    $result = $result . "<br />";
+    return $result;
+}
+
+function goodevening($showdayofweek = false){
+    $result = "Good Evening";
+    if ($showdayofweek) $result = $result . ", today is " . dayofweeknames(date('N'));
+    $result = $result . "<br />";
+    return $result;
+}
+
+$functionname = getgreetingfunction();
+echo $functionname();
+echo $functionname(true);
 
 
 
