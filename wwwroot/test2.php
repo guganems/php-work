3375fe8d4481a1cc7f312aca111f7dd0855bdac6
gnems <g_nemsveridze@cu.edu.ge>
2019-01-30 13:47:07 +0400

    Arrays

--- wwwroot/test2.php
+++ wwwroot/test2.php
@@ -9,3 +9,40 @@ define("WEBPAGEWIDTH", 100);
 
 echo WEBPAGEWIDTH;
 echo constant("WEBPAGEWIDTH");
+
+$salutation = "Good morning";
+echo $salutation . ", Today is " . date("l F jS \of Y") . "<br />";
+
+$somevariable = "Hello";
+$literalstring = "The $somevariable will not print its contents";
+
+print ($literalstring);
+print ("<br />");
+
+$literalstring = "$somevariable will print its contents";
+echo "$literalstring";
+
+$intnumbers = array(1, 2, 3, 4, 5);
+
+foreach ($intnumbers as $value){
+    echo "Array member value is $value <br />";
+}
+
+$letternumbers[0] = "one";
+$letternumbers[1] = "two";
+$letternumbers[2] = "three";
+$letternumbers[3] = "four";
+$letternumbers[4] = "five";
+
+foreach ($intnumbers as $value){
+    echo "Array member value is $value <br />";
+}
+
+
+
+
+
+
+
+
+
