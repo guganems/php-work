ba2bda7164f9e819044c31c54f279f6fad7a5ec4
gnems <g_nemsveridze@cu.edu.ge>
2019-01-30 14:23:17 +0400

    Break and Continue Sample

--- wwwroot/test2.php
+++ wwwroot/test2.php
@@ -107,6 +107,16 @@ foreach ($sheepsarray as $value) {
     echo "Sheep Number $value <br />";
 }
 
+$subtotal = 3.5;
+while (true){
+    $taxrate = rand(0, 10);
+    if ($taxrate == 10) break;
+
+    if ($taxrate == 0) continue;
+    $taxvalue = $subtotal * ($taxrate/100);
+    echo "Tax to be payed $taxvalue <br />";
+}
+
 
 
 
