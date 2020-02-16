14309409c7a65b3784a8de8f9bf3ce98bab442c4
gnems <g_nemsveridze@cu.edu.ge>
2020-02-16 14:30:58 +0400

    hm

--- 7.4.2/gdTransformAffineCopy.php
+++ 7.4.2/gdTransformAffineCopy.php
@@ -1,1 +1,10 @@
 <?php
+$src = imagecreatetruecolor(100, 100);
+imagefilledrectangle($src, 0, 0, 90, 90, 0xfffff);
+imageline($src, 0, 0, 90, 90, 0x00000);
+imagesetinterpolation($src, IMG_BSPLINE);
+$dst1 = imagerotate($src, 80, 0xffffff);
+imagepng($src, 'rot_exp.png');
+imageaffine($dst1, [1,1,1,1,1,1]);
+$dst2 = imagerotate($src, 80, 0xffffff);
+imagepng($dst2, 'rot_act.png');
